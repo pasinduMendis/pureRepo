2bceb5598bb5e0ee4d107d4b2f8531b3f76d767a
pasinduMendis <pasindu.mendis97@gmail.com>
2024-03-13 05:36:22 -0700

    add

---
+++ app/page.ts/page.js
@@ -0,0 +1,34 @@
+"use client";
+
+import "regenerator-runtime/runtime";
+import React, { useEffect, useState } from "react";
+import App from "../../components/App";
+
+function Page() {
+  const [pathname, setPathname] = useState("");
+  const [loading, setLoading] = useState(true);
+  //get the pathname from the URL
+  useEffect(() => {
+    if (window !== undefined) {
+      const pathname = window.location.pathname;
+      setLoading(false);
+      setPathname(pathname);
+    } else {
+      setLoading(true);
+    }
+  }, []);
+
+  return (
+    <>
+   <title>Wrappeer</title>
+        <meta
+          name="description"
+          content={`test description`}
+        />
+        <meta property="og:image" content="https://ttimages.blob.core.windows.net/property/21e11ce8-e391-4aa5-b872-c94cfbbe45f1.jpg" />
+    test
+    </>
+  );
+}
+
+export default Page;
