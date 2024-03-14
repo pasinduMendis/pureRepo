ca02b649e9573871740c9d1de941981596737d58
pasinduMendis <pasindu.mendis97@gmail.com>
2024-03-14 10:04:58 -0700

    add

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 function Page() {
-  const [pathname, setPathname] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [metaTitle, setMetaTitle] = useState("");
-  const [metaDescription, setMetaDescription] = useState("");
-  const [metaImage, setMetaImage] = useState("");
   const rouute = useRouter();
   //get the pathname from the URL
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -17,7 +12,7 @@ function Page() {
     rouute.push("/searching");
   }, []);
 
-  return <>searching....</>;
+  return <></>;
 }
 
 export default Page;
